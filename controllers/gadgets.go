8a8a1dbc45da3962d72bbeb67f0f7f88fc1f2b62
Craig Swank <craigswank@gmail.com>
2014-04-12 17:50:00 -0600

    got brewery working without a password

--- controllers/gadgets.go
+++ controllers/gadgets.go
@@ -8,7 +8,7 @@ import (
 
 func GetGadgets(w http.ResponseWriter, r *http.Request) error {
 	gadgets := models.GetGadgets()
-	b, err := json.Marshal(gadgets)
+	b, err := json.Marshal(map[string][]models.Gadget{"gadgets": gadgets})
 	if err != nil {
 		return err
 	}
