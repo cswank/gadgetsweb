905199616eb1aa3050301f92c5030a59daf5c0b2
Craig Swank <craigswank@gmail.com>
2014-05-14 16:10:08 -0600

    got login/logout down

--- static/app/js/controllers.js
+++ static/app/js/controllers.js
@@ -1,38 +1,6 @@
 'use strict';
 
-angular.module('myApp.controllers', []).
-    controller('NavbarCtrl', ['$scope', '$modal', 'gadgets', 'auth', function($scope, $modal, gadgets, auth) {
-
-        function login() {
-            if ($scope.username == undefined) {
-                var dlg = $modal.open({
-                    templateUrl: '/dialogs/login.html?c=' + new Date().getTime(),
-                    controller: LoginCtrl,
-                });
-                dlg.result.then(function(user) {
-                    $scope.username = user.name;
-                    $scope.password = user.password;
-                    auth.login($scope.username, $scope.password, function(){
-                        getGadgets();
-                    });
-                });
-            } else {
-                auth.login($scope.username, $scope.password, function(){
-                    getGadgets();
-                });
-            }
-        }
-
-        function getGadgets() {
-            $scope.gadgets = gadgets.get(function(data) {
-                $scope.gadgets = data.gadgets;
-            }, function() {
-                console.log("get gadgets failed");
-                login();
-            });
-        }
-        getGadgets();
-    }])
+angular.module('myApp.controllers', [])
     .controller('GadgetsCtrl', ['$rootScope', '$scope', '$routeParams', 'sockets', function($rootScope, $scope, $routeParams, sockets) {
         $scope.locations = {live:false};
         $scope.name = $routeParams.gadget;
